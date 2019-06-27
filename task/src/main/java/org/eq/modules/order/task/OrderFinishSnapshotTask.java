a8e343dafd1a317a7a3fcd0841b62e0e638f8842
bo.gao <bo.gao@mtime.com>
2019-06-27 14:47:27 +0800

    修复定时任务调度时间

--- task/src/main/java/org/eq/modules/order/task/OrderFinishSnapshotTask.java
+++ task/src/main/java/org/eq/modules/order/task/OrderFinishSnapshotTask.java
@@ -22,7 +22,7 @@ public class OrderFinishSnapshotTask {
     private final OrderFinishSnapshotBiz biz;
 
 
-    @Scheduled(cron = "* 0/1 * * * ?")
+    @Scheduled(cron = "* 0/5 * * * ?")
     public void process() {
         log.info("每分钟执行");
         biz.searchFinishOrderAd().forEach(o->{
